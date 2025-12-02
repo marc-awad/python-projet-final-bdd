cb4d3501ecdc83d0faff8bdd1ce8f61e85d8e221
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-12-02 15:10:57 +0100

    test: ajout de tests pour les fonctions utils.py dans main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from models import Personnage, Monstre
+from utils import get_all_personnages, get_random_monstre
 
 client = MongoClient("mongodb://localhost:27017")
 db = client["jeu_video"]
@@ -10,3 +11,11 @@ m = Monstre("Gobelin", 10, 5, 50)
 
 print(p)
 print(m)
+
+# Tester la récupération de tous les personnages
+for p in get_all_personnages():
+    print(p)
+
+
+# Tester la récupération d'un monstre aléatoire
+print(get_random_monstre())
