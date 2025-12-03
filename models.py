67494cb4a6e728a066654cc2170f00c1bd039328
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-12-03 15:41:13 +0100

    refactor: classes heritage from entity classe

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-class Personnage:
+class Entity:
     def __init__(self, nom, atk, defense, pv):
         self.nom = nom
         self.atk = atk
@@ -23,26 +23,11 @@ class Personnage:
         return f"{self.nom} - ATK: {self.atk}, DEF: {self.defense}, PV: {self.pv}/{self.pv_max}"
 
 
-class Monstre:
+class Personnage(Entity):
     def __init__(self, nom, atk, defense, pv):
-        self.nom = nom
-        self.atk = atk
-        self.defense = defense
-        self.pv = pv
-        self.pv_max = pv
+        super().__init__(nom, atk, defense, pv)
 
-    def attaquer(self, cible):
-        degats = max(self.atk - cible.defense, 0)
-        cible.subir_degats(degats)
-        return degats
 
-    def subir_degats(self, valeur):
-        self.pv -= valeur
-        if self.pv < 0:
-            self.pv = 0
-
-    def est_vivant(self):
-        return self.pv > 0
-
-    def __str__(self):
-        return f"{self.nom} - ATK: {self.atk}, DEF: {self.defense}, PV: {self.pv}/{self.pv_max}"
+class Monstre(Entity):
+    def __init__(self, nom, atk, defense, pv):
+        super().__init__(nom, atk, defense, pv)
